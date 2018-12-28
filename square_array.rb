696aa897539a07a4a63c26cdb9fa2fc952616e56
Indre Bakeer <indre.bakeer@gmail.com>
2018-12-28 06:56:35 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,11 @@
 def square_array(numbers)
 new_numbers = []
+<<<<<<< HEAD
 numbers.each do |num|
 new_numbers << (num ** 2)
 end
+=======
+numbers.each do {|num| new_numbers << (num ** 2)}
+>>>>>>> ef9fc55aa994262870ef6067bfa736b73f6c9a2e
 new_numbers
 end
